fd527b92a0e924574c9fc3b63409d1773ac0cec9
Ryan Pepper <ryan.pepper@soton.ac.uk>
2018-10-26 14:08:56 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,4 @@
+if __debug__:
+    print('This code will be run in debug mode')
+else:
+    print('This code will be run when in optimised mode')
---
+++ test_functions.py
@@ -0,0 +1,11 @@
+
+def square(x):
+    return x*x
+
+
+def test_square_positive_integers():
+    assert square(1) == 1
+    assert square(2) == 4
+    assert square(4) == 16
+    assert square(100) == 10000
+    assert 2 == 1
